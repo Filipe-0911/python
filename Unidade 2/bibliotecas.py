62b1fefdd32add69367114b9d6fbdfe95faa34b0
Filipe de Bianchi Andrade <fbianchi.andrade@gmail.com>
2023-09-13 20:10:15 -0300

    utilizando bibliotecas math e random

---
+++ Unidade 2/bibliotecas.py
@@ -0,0 +1,13 @@
+import math
+import random #numeros aleatorios
+
+numero = 6.9
+numero = round(numero, 2) #round arredonda o numero
+
+# print(numero)
+# print(math.floor(numero)) #arredonda sempre pra baixo
+# print(math.ceil(numero)) #arredonda sempre pra cima
+# print(math.pi)
+
+print(round(random.random() * 100)) #gera numeros aleatorios inteiros devido ao round
+print(random.randint(1, 20)) #gera numeros aleatorios inteiros
