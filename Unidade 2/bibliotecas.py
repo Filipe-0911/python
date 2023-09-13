6e15e271cf99c243fc3da685e10b3617dab999a0
Filipe de Bianchi Andrade <fbianchi.andrade@gmail.com>
2023-09-13 20:10:24 -0300

    utilizando bibliotecas math e random

--- Unidade 2/bibliotecas.py
+++ Unidade 2/bibliotecas.py
@@ -4,10 +4,10 @@ import random #numeros aleatorios
 numero = 6.9
 numero = round(numero, 2) #round arredonda o numero
 
-# print(numero)
-# print(math.floor(numero)) #arredonda sempre pra baixo
-# print(math.ceil(numero)) #arredonda sempre pra cima
-# print(math.pi)
+print(numero)
+print(math.floor(numero)) #arredonda sempre pra baixo
+print(math.ceil(numero)) #arredonda sempre pra cima
+print(math.pi)
 
 print(round(random.random() * 100)) #gera numeros aleatorios inteiros devido ao round
 print(random.randint(1, 20)) #gera numeros aleatorios inteiros
