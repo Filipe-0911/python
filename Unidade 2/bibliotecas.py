695c4476250093e7b6551ec0428decbe278b1d0f
Filipe de Bianchi Andrade <fbianchi.andrade@gmail.com>
2023-09-13 20:20:52 -0300

    instalando biblioteca NumPy

--- Unidade 2/bibliotecas.py
+++ Unidade 2/bibliotecas.py
@@ -1,5 +1,6 @@
 import math
 import random #numeros aleatorios
+import numpy as np
 
 numero = 6.9
 numero = round(numero, 2) #round arredonda o numero
@@ -11,3 +12,7 @@ print(math.pi)
 
 print(round(random.random() * 100)) #gera numeros aleatorios inteiros devido ao round
 print(random.randint(1, 20)) #gera numeros aleatorios inteiros
+
+a = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
+
+print(a)
