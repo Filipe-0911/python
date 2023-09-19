61c129f9a1e89cf0e38f7354449cce2a4b1b7e7e
Filipe de Bianchi Andrade <fbianchi.andrade@gmail.com>
2023-09-19 18:48:50 -0300

    criando grafico com plt com lista ordenada, atraves do pd, pelo IMC

--- Unidade 2/saude.py
+++ Unidade 2/saude.py
@@ -17,13 +17,26 @@ pacientes = {
     "Paciente 4" : Paciente('Lemoel', 55, 'Masculino', 110, 1.83),
 }
 
-l_pacientes = [p.__dict__ for p in pacientes.values()] #criando lista de pacientes
-df_pacientes = pd.DataFrame.from_records(l_pacientes, index = pacientes.keys()) #criando DataFrame (tabela)
-df_pacientes['IMC'] = df_pacientes.apply(lambda i: i.peso / i.altura ** 2, axis = 1) #calculando imc df_pacientes
-media = np.mean(df_pacientes['IMC']) #media do imc do df_pacientes
-sobrepeso = df_pacientes[df_pacientes['IMC'] > 25] #verificando quantos estao com imc acima de 25
-percentual = len(sobrepeso) / len(df_pacientes) * 100 #percentual
-ordenada = df_pacientes.sort_values(by='IMC') #ordenando do menor pro maior com pandas
+#criando lista de pacientes
+l_pacientes = [p.__dict__ for p in pacientes.values()]
+
+#criando DataFrame (tabela)
+df_pacientes = pd.DataFrame.from_records(l_pacientes, index = pacientes.keys())
+
+#calculando imc df_pacientes axis = 1 significa a partir do eixo 1
+df_pacientes['IMC'] = df_pacientes.apply(lambda i: i.peso / i.altura ** 2, axis = 1)
+
+#media do imc do df_pacientes
+media = np.mean(df_pacientes['IMC'])
+
+#verificando quantos estao com imc acima de 25
+sobrepeso = df_pacientes[df_pacientes['IMC'] > 25]
+
+#percentual
+percentual = len(sobrepeso) / len(df_pacientes) * 100
+
+#ordenando do menor pro maior com pandas
+ordenada = df_pacientes.sort_values(by='IMC') 
 
 plt.bar(ordenada['nome'], ordenada['IMC'] , label='Paciente', color='green')
 plt.title('Peso de cada paciente')
